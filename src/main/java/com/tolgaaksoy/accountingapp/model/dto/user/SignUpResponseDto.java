de671311f0d6602bcde137d56e72288f82b0dd44
Tolga Aksoy <dev.tolgaaksoy@gmail.com>
2022-01-21 18:12:22 +0300

    Fix(User): user dtos
    Why:
    Necessity of name and surname fields in user dtos.
    
    How:
    Add name and surname fields in user dtos.
    
    Tags: User, name, surname, dto

--- src/main/java/com/tolgaaksoy/accountingapp/model/dto/user/SignUpResponseDto.java
+++ src/main/java/com/tolgaaksoy/accountingapp/model/dto/user/SignUpResponseDto.java
@@ -1,9 +1,12 @@
 package com.tolgaaksoy.accountingapp.model.dto.user;
 
+import com.tolgaaksoy.accountingapp.model.entity.user.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Set;
 
 @Data
 @Builder
@@ -12,4 +15,5 @@ import lombok.NoArgsConstructor;
 public class SignUpResponseDto {
     private String token;
     private String tokenType = "Bearer";
+    private Set<Role> roleList;
 }
